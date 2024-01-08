e34486be7be653b6984b243b02d66ee2802f137b
JinhyeokBang <jinhyeokfang@gmail.com>
2024-01-08 21:52:07 +0900

    hotfix: 빌드 오류 수정 #5 Fix #5

---
+++ src/main/java/uk/jinhy/survey_mate_api/SurveyMateApplication.java
@@ -0,0 +1,11 @@
+package uk.jinhy.survey_mate_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyMateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SurveyMateApplication.class, args);
+    }
+}
---
+++ src/test/java/uk/jinhy/survey_mate_api/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.jinhy.survey_mate_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
