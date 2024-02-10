ebce83ff7617a8e3b5567abe7f807e1f542bdc6e
JinhyeokBang <jinhyeokfang@gmail.com>
2024-02-10 14:35:07 +0900

    refactor: Service 정의 구현 분리

--- src/main/java/uk/jinhy/survey_mate_api/survey/application/service/SurveyService.java
+++ src/main/java/uk/jinhy/survey_mate_api/survey/application/service/SurveyService.java
@@ -1,122 +1,30 @@
 package uk.jinhy.survey_mate_api.survey.application.service;
 
-import jakarta.transaction.Transactional;
-import java.time.LocalDateTime;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 import uk.jinhy.survey_mate_api.auth.domain.entity.Member;
-import uk.jinhy.survey_mate_api.common.response.Status;
-import uk.jinhy.survey_mate_api.common.response.exception.GeneralException;
-import uk.jinhy.survey_mate_api.common.util.Util;
 import uk.jinhy.survey_mate_api.survey.application.dto.SurveyServiceDTO;
-import uk.jinhy.survey_mate_api.survey.domain.entity.Answer;
+import uk.jinhy.survey_mate_api.survey.application.dto.SurveyServiceDTO.CreateSurveyDTO;
 import uk.jinhy.survey_mate_api.survey.domain.entity.Survey;
-import uk.jinhy.survey_mate_api.survey.domain.repository.SurveyRepository;
 
-@RequiredArgsConstructor
-@Service
-public class SurveyService {
+public interface SurveyService {
 
-    private final SurveyRepository surveyRepository;
+    void createSurvey(Member registrant, CreateSurveyDTO dto);
 
-    public Survey createSurvey(Member registrant, SurveyServiceDTO.CreateSurveyDTO dto) {
-        Survey survey = Survey.builder()
-            .reward(dto.getReward())
-            .endedAt(LocalDateTime.now().plusDays(dto.getPeriod()))
-            .linkUrl(dto.getLinkUrl())
-            .rewardUrl(Util.generateRandomString(10))
-            .title(dto.getTitle())
-            .description(dto.getDescription())
-            .registrant(registrant)
-            .build();
-        surveyRepository.save(survey);
-        return survey;
-    }
+    void editSurvey(Member registrant, SurveyServiceDTO.EditSurveyDTO dto);
 
-    @Transactional
-    public void editSurvey(Member registrant, SurveyServiceDTO.EditSurveyDTO dto) {
-        Long surveyId = dto.getSurveyId();
-        Survey survey = surveyRepository.findBySurveyId(surveyId).orElseThrow(
-            () -> new GeneralException(Status.SURVEY_NOT_FOUND)
-        );
+    void deleteSurvey(Member registrant, Long surveyId);
 
-        if (!survey.getRegistrant().equals(registrant)) {
-            throw new GeneralException(Status.WRONG_REGISTRANT);
-        }
+    void addAnswer(Member respondent, String rewardUrl);
 
-        String newTitle = dto.getTitle();
-        if (newTitle != null) {
-            survey.updateTitle(newTitle);
-        }
+    Survey getSurvey(Long surveyId);
 
-        String newDescription = dto.getDescription();
-        if (newDescription != null) {
-            survey.updateDescription(newDescription);
-        }
+    List<Survey> getSurveyList(int pageNumber);
 
-        String newLinkUrl = dto.getLinkUrl();
-        if (newLinkUrl != null) {
-            survey.updateLinkUrl(newLinkUrl);
-        }
-    }
+    List<Survey> getMySurveyList(Member registrant);
 
-    @Transactional
-    public void deleteSurvey(Member registrant, Long surveyId) {
-        Survey survey = surveyRepository.findBySurveyId(surveyId).orElseThrow(
-            () -> new GeneralException(Status.SURVEY_NOT_FOUND)
-        );
+    List<Survey> getAnsweredSurveyList(Member respondent);
 
-        if (!survey.getRegistrant().equals(registrant)) {
-            throw new GeneralException(Status.WRONG_REGISTRANT);
-        }
+    List<Survey> getRecentSurveyList();
 
-        surveyRepository.deleteById(surveyId);
-    }
-
-    public void addAnswer(Member respondent, String rewardUrl) {
-        Survey survey = surveyRepository.findByRewardUrl(rewardUrl).orElseThrow(
-            () -> new GeneralException(Status.SURVEY_NOT_FOUND)
-        );
-        if (survey.isAnswered(respondent)) {
-            throw new GeneralException(Status.ALREADY_ANSWERED);
-        }
-        Answer answer = Answer.builder()
-            .survey(survey)
-            .respondent(respondent)
-            .build();
-        survey.addAnswer(answer);
-        surveyRepository.save(survey);
-    }
-
-    public Survey getSurvey(Long surveyId) {
-        return surveyRepository.findBySurveyId(surveyId)
-            .orElseThrow(() -> new GeneralException(Status.SURVEY_NOT_FOUND));
-    }
-
-    public List<Survey> getSurveyList(int pageNumber) {
-        Pageable pageable = PageRequest.of(pageNumber, 10);
-        return surveyRepository.findByEndedAtAfterOrderByEndedAt(
-            LocalDateTime.now(),
-            pageable
-        );
-    }
-
-    public List<Survey> getMySurveyList(Member registrant) {
-        return surveyRepository.findByRegistrant(registrant);
-    }
-
-    public List<Survey> getAnsweredSurveyList(Member respondent) {
-        return surveyRepository.findByRespondent(respondent);
-    }
-
-    public List<Survey> getRecentSurveyList() {
-        return surveyRepository.findRecentSurvey();
-    }
-
-    public boolean isResponded(Survey survey, Member member) {
-        return survey.isAnswered(member);
-    }
+    boolean isResponded(Survey survey, Member member);
 }
---
+++ src/main/java/uk/jinhy/survey_mate_api/survey/application/service/SurveyServiceFacadeImpl.java
@@ -0,0 +1,41 @@
+package uk.jinhy.survey_mate_api.survey.application.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import uk.jinhy.survey_mate_api.auth.domain.entity.Member;
+import uk.jinhy.survey_mate_api.statement.application.dto.StatementServiceDTO;
+import uk.jinhy.survey_mate_api.statement.application.service.StatementService;
+import uk.jinhy.survey_mate_api.survey.application.dto.SurveyServiceDTO;
+
+@RequiredArgsConstructor
+@Service
+public class SurveyServiceFacadeImpl implements SurveyServiceFacade {
+
+    private final SurveyService surveyService;
+    private final StatementService statementService;
+
+    @Transactional
+    public void createSurvey(Member registrant, SurveyServiceDTO.CreateSurveyDTO dto) {
+        StatementServiceDTO.PayPointDTO payPointDTO = StatementServiceDTO
+            .PayPointDTO
+            .builder()
+            .description("설문조사 추가")
+            .amount(SurveyRegistrationFee.getFee(dto.getPeriod()))
+            .build();
+        surveyService.createSurvey(registrant, dto);
+        statementService.payPoint(registrant, payPointDTO);
+    }
+
+    @Transactional
+    public void answerSurvey(Member respondent, String rewardUrl) {
+        StatementServiceDTO.EarnPointDTO earnPointDTO = StatementServiceDTO
+            .EarnPointDTO
+            .builder()
+            .description("설문조사 응답")
+            .amount(1L)
+            .build();
+        surveyService.addAnswer(respondent, rewardUrl);
+        statementService.earnPoint(respondent, earnPointDTO);
+    }
+}
