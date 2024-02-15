c7786c51929db7ab772bd9b7e19ca861731d4a39
JinhyeokBang <jinhyeokfang@gmail.com>
2024-02-15 19:36:37 +0900

    hotfix: mail 기능 작업

--- src/main/java/uk/jinhy/survey_mate_api/common/email/infrastructure/MailSenderImpl.java
+++ src/main/java/uk/jinhy/survey_mate_api/common/email/infrastructure/MailSenderImpl.java
@@ -1,18 +1,20 @@
 package uk.jinhy.survey_mate_api.common.email.infrastructure;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
+import com.mailjet.client.ClientOptions;
+import com.mailjet.client.MailjetClient;
+import com.mailjet.client.MailjetRequest;
+import com.mailjet.client.MailjetResponse;
+import com.mailjet.client.errors.MailjetException;
+import com.mailjet.client.resource.Emailv31;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 import uk.jinhy.survey_mate_api.common.email.service.MailSender;
 import uk.jinhy.survey_mate_api.common.email.service.dto.MailSenderDTO.SendMailDTO;
-import uk.jinhy.survey_mate_api.common.response.Status;
-import uk.jinhy.survey_mate_api.common.response.exception.GeneralException;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -24,21 +26,35 @@ public class MailSenderImpl implements MailSender {
 
     @Override
     public void sendMail(SendMailDTO dto) {
+        MailjetRequest request;
+        MailjetResponse response;
+
+        ClientOptions options = ClientOptions.builder()
+            .apiKey(System.getenv("MAIL_ACCESS_KEY"))
+            .apiSecretKey(System.getenv("MAIL_SECRET_KEY"))
+            .build();
+
+        MailjetClient client = new MailjetClient(options);
+
+        request = new MailjetRequest(Emailv31.resource)
+            .property(Emailv31.MESSAGES, new JSONArray()
+                .put(new JSONObject()
+                    .put(Emailv31.Message.FROM, new JSONObject()
+                        .put("Email", "no_reply@jinhy.uk")
+                        .put("Name", "썰매(Survey Mate)"))
+                    .put(Emailv31.Message.TO, new JSONArray()
+                        .put(new JSONObject()
+                            .put("Email", dto.getTo())
+                            .put("Name", "You")))
+                    .put(Emailv31.Message.SUBJECT, dto.getSubject())
+                    .put(Emailv31.Message.HTMLPART, dto.getContent())));
         try {
-            MimeMessage message = emailSender.createMimeMessage();
-            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message, true, "UTF-8");
-            mimeMessageHelper.setFrom(dto.getFrom());
-            mimeMessageHelper.setTo(dto.getTo());
-            mimeMessageHelper.setSubject(dto.getSubject());
-            mimeMessageHelper.setText(dto.getContent(), true);
-            mimeMessageHelper.addInline("logo", new ClassPathResource("images/logo.png"));
-            emailSender.send(message);
-        } catch (RuntimeException e) {
-            log.error(e.getMessage());
-            throw new GeneralException(Status.MAIL_SEND_FAIL);
-        } catch (MessagingException e) {
-            log.error(e.getMessage());
+            response = client.post(request);
+        } catch (MailjetException e) {
             throw new RuntimeException(e);
         }
+        System.out.println(response.getStatus());
+        System.out.println(response.getData());
     }
 }
+
