445b6525f1f2bae45dca27a28d8e87577eb87aeb
JinhyeokBang <jinhyeokfang@gmail.com>
2024-02-17 22:59:18 +0900

    feat: 도메인 변경 반영

--- src/main/java/uk/jinhy/survey_mate_api/statement/presentation/StatementController.java
+++ src/main/java/uk/jinhy/survey_mate_api/statement/presentation/StatementController.java
@@ -4,21 +4,19 @@ import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.jinhy.survey_mate_api.auth.application.service.AuthService;
 import uk.jinhy.survey_mate_api.auth.domain.entity.Member;
 import uk.jinhy.survey_mate_api.common.response.ApiResponse;
 import uk.jinhy.survey_mate_api.common.response.Status;
-import uk.jinhy.survey_mate_api.common.response.exception.GeneralException;
 import uk.jinhy.survey_mate_api.statement.application.service.StatementService;
 import uk.jinhy.survey_mate_api.statement.domain.entity.Statement;
 import uk.jinhy.survey_mate_api.statement.presentation.converter.StatementConverter;
 import uk.jinhy.survey_mate_api.statement.presentation.dto.StatementControllerDTO;
 
 @RequiredArgsConstructor
-@RequestMapping("/statement")
+@RequestMapping("/api/statement")
 @RestController
 public class StatementController {
 
@@ -33,7 +31,8 @@ public class StatementController {
         Member member = authService.getCurrentMember();
 
         List<Statement> statementList = statementService.getStatementList(member);
-        StatementControllerDTO.StatementListDTO responseDTO = converter.toControllerStatementListDto(statementList);
+        StatementControllerDTO.StatementListDTO responseDTO = converter.toControllerStatementListDto(
+            statementList);
 
         return ApiResponse.onSuccess(Status.OK.getCode(), Status.OK.getMessage(), responseDTO);
     }
@@ -44,7 +43,8 @@ public class StatementController {
         Member member = authService.getCurrentMember();
 
         List<Statement> statementList = statementService.getStatementListAsBuyer(member);
-        StatementControllerDTO.StatementListDTO responseDTO = converter.toControllerStatementListDto(statementList);
+        StatementControllerDTO.StatementListDTO responseDTO = converter.toControllerStatementListDto(
+            statementList);
 
         return ApiResponse.onSuccess(Status.OK.getCode(), Status.OK.getMessage(), responseDTO);
     }
@@ -55,7 +55,8 @@ public class StatementController {
         Member member = authService.getCurrentMember();
 
         List<Statement> statementList = statementService.getStatementListAsSeller(member);
-        StatementControllerDTO.StatementListDTO responseDTO = converter.toControllerStatementListDto(statementList);
+        StatementControllerDTO.StatementListDTO responseDTO = converter.toControllerStatementListDto(
+            statementList);
 
         return ApiResponse.onSuccess(Status.OK.getCode(), Status.OK.getMessage(), responseDTO);
     }
